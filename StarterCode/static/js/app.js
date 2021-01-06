cd25dcf45f2be89a6cd8a6fcab28ff34923c9565
Michael Alread <malreadjr217@gmail.com>
2021-01-05 21:32:07 -0500

    metadata chart added

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,21 +1,43 @@
+window.data = null;
+
 d3.json("samples.json").then((importedData) => {
       // console.log(importedData);
-      var data = importedData;
+      data = importedData;
+	  //alert(data)
      
       var selectElement = document.getElementById('selDataset');
-
-
+	  selectElement.add(new Option("")); 
       for (i = 0; i <  importedData['samples'].length; i++) {
-        selectElement.add(new Option(importedData['samples'][i]['id']));
-        
-      } 
-      
+        selectElement.add(new Option(importedData['samples'][i]['id']));        
+      }      
       
     
 });
 
 function optionChanged(selectValue) {
-    alert(selectValue)
+    let selectedResult = data['metadata'].find(i => i.id === parseInt(selectValue));
 
-    
+let id = selectedResult.id;
+let ethnicity = selectedResult.ethnicity;
+let gender = selectedResult.gender;
+let age = selectedResult.age;
+let location = selectedResult.location;
+let bbtype = selectedResult.bbtype;
+let wfreq = selectedResult.wfreq;
+
+
+var metaDataElement = document.getElementById('sample-metadata');
+metaDataElement.innerHTML = "";
+metaDataElement.innerHTML += "<p>id: " + id + "</p>"
+metaDataElement.innerHTML += "<p>ethnicity:  " + ethnicity + "</p>"
+metaDataElement.innerHTML += "<p>gender:  " + gender + "</p>"
+metaDataElement.innerHTML += "<p>age: " + age + "</p>"
+metaDataElement.innerHTML += "<p>bbtype:  " + bbtype + "</p>"
+metaDataElement.innerHTML += "<p>location: " + location + "</p>"
+metaDataElement.innerHTML += "<p>wfreq:  " + wfreq + "</p>"
+  
 }
+
+function getKeyByValue(object, value) { 
+    return Object.keys(object).find(key => object[key] === value); 
+} 
